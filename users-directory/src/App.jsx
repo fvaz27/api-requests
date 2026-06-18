33a163e70231244d66e309b67444a7a8c08b2ae8
Daniel Sullivan <Daniel.Sullivan83@Stu.Bmcc.Cuny.Edu>
2026-06-18 14:55:20 -0400

    Step 2

--- users-directory/src/App.jsx
+++ users-directory/src/App.jsx
@@ -7,9 +7,17 @@ import './App.css'
 
 
 export default function App() {
-  
-  useEffect(()=>{
-    console.log("page loaded")
+ const [users, setUsers] = useState([]) 
+
+ useEffect(()=>{
+    console.log("page loaded") 
+    async function fetchUsers() {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        console.log(data)
+        setUsers(users)
+  }
+  fetchUsers()
   }, [])
 
   
